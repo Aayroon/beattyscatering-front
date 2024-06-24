a521e3e5a4a2a7aefe23f89048b2eb05bca6356f
Aayroon <aaronitez4@gmail.com>
2024-06-24 18:13:42 +0300

    Home page text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,29 +28,29 @@ export default function Home() {
               <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
                 <div className="hidden sm:mb-10 sm:flex">
                   <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                    Anim aute id magna aliqua ad ad non deserunt sunt.{' '}
+                    Bringing gourmet experiences to your celebrations.{' '}
                     <a href="#" className="whitespace-nowrap font-semibold text-indigo-600">
                       <span className="absolute inset-0" aria-hidden="true" />
-                      Read more <span aria-hidden="true">&rarr;</span>
+                      Why choose us <span aria-hidden="true">&rarr;</span>
                     </a>
                   </div>
                 </div>
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                  Data to enrich your online business
+                  Savor the Taste of Excellence
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
-                  amet fugiat veniam occaecat fugiat aliqua.
+                  From intimate gatherings to grand celebrations, we provide top-notch catering services for weddings, corporate events, parties, and more.
+                  Our dedicated team brings culinary artistry and exceptional service to your special events, making them truly unforgettable.
                 </p>
                 <div className="mt-10 flex items-center gap-x-6">
                   <a
                     href="#"
                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    Get started
+                    Get a Quote
                   </a>
                   <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                    Learn more <span aria-hidden="true">→</span>
+                    Explore Services <span aria-hidden="true">→</span>
                   </a>
                 </div>
               </div>
